47c7443559da56b44fb216f32c8cd9f03ed94591
executive-eachle <saatdeniz@gmail.com>
2017-06-03 18:24:14 -0400

    feat: Second Commit
    Added game board (dictionary) and symbol list.
    Includes test for time count down on lines 6-22.

--- game_test.py
+++ game_test.py
@@ -1,3 +1,36 @@
 #!usr/bin/env python3
 
-print('test')
+# This module will allow us to time the user
+import time
+
+'''
+print('You have 5 seconds to guess correctly.')
+
+mario = True
+while mario:
+	second = 1
+	for second in range(1, 6, 1):
+		time.sleep(1)
+		#print(second)
+		user_response = input('> Where is Mario?')
+		second += 1
+
+	mario = False
+	
+	
+print('Mario is dead.')
+'''
+
+# The board
+board = {'top-l': '', 'top-m': '', 'top-r': '',
+		 'mid-l': '', 'mid-m': '', 'mid-r': '',
+		 'low-l': '', 'low-m': '', 'low-r': ''}
+
+symbol_list = [u'\u2660', u'\u2663', u'\u2666', u'\u2665', u'\u263b']
+
+print(symbol_list)
+
+
+
+
+
