59888be90fc40e51db4dab3e9f9abb216fd50b4f
executive-eachle <saatdeniz@gmail.com>
2017-06-10 19:04:17 -0400

    feat: printBoard
    Added in printBoard function

--- game_test.py
+++ game_test.py
@@ -2,6 +2,7 @@
 
 # This module will allow us to time the user
 import time
+import random
 
 '''
 print('You have 5 seconds to guess correctly.')
@@ -25,12 +26,48 @@ print('Mario is dead.')
 board = {'top-l': '', 'top-m': '', 'top-r': '',
 		 'mid-l': '', 'mid-m': '', 'mid-r': '',
 		 'low-l': '', 'low-m': '', 'low-r': ''}
+def printBoard(board):
+	print(board['top-l'] + '|' + board['top-m'] + '|' + board['top-r'])
+	print('-+-+-')
+	print(board['mid-l'] + '|' + board['mid-m'] + '|' + board['mid-r'])
+	print('-+-+-')
+	print(board['low-l'] + '|' + board['low-m'] + '|' + board['low-r'])
+	
 
 symbol_list = [u'\u2660', u'\u2663', u'\u2666', u'\u2665', u'\u263b']
 
 print(symbol_list)
+printBoard(board)
 
-
+def placer(board):
+	s = list(range(9))
+	print(s)
+	#Shuffles the list
+	q = s.copy()
+	random.shuffle(q)
+	
+	for k, v in board.items():
+		for num in q:
+			if num in [0, 1]:
+				board[k] = symbol_list[0]
+				print(board[k])
+			elif num in [2, 3]:
+				board[k] = symbol_list[1]
+				print(board[k])
+			elif num in [4, 5]:
+				board[k] = symbol_list[2]
+				print(board[k])
+			elif num in [6, 7]:
+				board[k] = symbol_list[3]
+				print(board[k])
+			else:
+				board[k] = symbol_list[4]
+				
+	print(board)
+placer(board)
+		
+		
+		
 
 
 
